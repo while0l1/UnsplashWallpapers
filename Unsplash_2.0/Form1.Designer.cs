38ce36bda0f6893405dd20a120a65d8f881680a2
while0l1 <1255015689@qq.com>
2019-11-18 17:52:54 +0800

    添加项目文件。

---
+++ Unsplash_2.0/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Unsplash_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.更新壁纸ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_Click);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.更新壁纸ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 70);
+            // 
+            // 更新壁纸ToolStripMenuItem
+            // 
+            this.更新壁纸ToolStripMenuItem.Name = "更新壁纸ToolStripMenuItem";
+            this.更新壁纸ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.更新壁纸ToolStripMenuItem.Text = "更新壁纸";
+            this.更新壁纸ToolStripMenuItem.Click += new System.EventHandler(this.更新壁纸ToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.保存ToolStripMenuItem.Text = "保存壁纸";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 93);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Unsplash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_AutoSizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 更新壁纸ToolStripMenuItem;
+    }
+}
+
---
+++ Unsplash_2.0/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Unsplash_2._0
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+
+        public static extern int SystemParametersInfo(UAction uAction, int uParam, StringBuilder lpvParam, int fuWinIni);
+
+        string workingpath = System.IO.Directory.GetCurrentDirectory();
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Visible = true;    //显示托盘图标
+            System.Timers.Timer aTimer = new System.Timers.Timer
+            {
+                Interval = 120000,
+                Enabled = true
+            };
+            aTimer.Start();
+            aTimer.Elapsed += new ElapsedEventHandler(AutoSetWallPaper);
+        }
+
+        public enum UAction
+        {
+            /// <summary>
+            /// set the desktop background image
+            /// </summary>
+            SPI_SETDESKWALLPAPER = 0x0014,
+            /// <summary>
+            /// set the desktop background image
+            /// </summary>
+            SPI_GETDESKWALLPAPER = 0x0073,
+        }
+
+        private void Form1_Load(object sender, EventArgs e)  //默认最小化
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+        }
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)  //双击托盘显示界面，不显示界面
+        {
+            //this.Visible = true;
+            //this.ShowInTaskbar = true;
+            //this.Show();
+            //this.Focus();
+        }
+
+        private void Form1_AutoSizeChanged(object sender, EventArgs e)  //最小化的时候隐藏到托盘
+        {
+            if (this.WindowState == FormWindowState.Minimized)    //最小化到系统托盘
+            {
+                this.Hide();    //隐藏窗口
+            }
+        }
+        //退出软件
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Application.Exit();
+        }
+
+        //手动更新壁纸
+        private void 更新壁纸ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Thread fetch_set = new Thread(Downpng_set);
+            fetch_set.Start();
+        }
+
+        //保存当前壁纸
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            var wallpapername = dt.GetHashCode().ToString();
+            wallpapername = wallpapername.Substring(1);
+            string filepath = workingpath + "\\Picture\\" + wallpapername + ".png";
+            try
+            {
+                File.Move(workingpath + "\\Picture\\temp.png", filepath);
+            }
+            catch
+            {
+                MessageBox.Show("壁纸已保存");
+            }
+        }
+
+        //任务栏图标右键操作
+        private void NotifyIcon1_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show();
+            }
+        }
+
+        public static string GetBackgroud()
+        {
+            StringBuilder s = new StringBuilder(300);
+            SystemParametersInfo(UAction.SPI_GETDESKWALLPAPER, 300, s, 0);
+            return s.ToString();
+        }
+
+        public static int SetBackgroud(string fileName)
+        {
+            int result = 0;
+            if (File.Exists(fileName))
+            {
+                StringBuilder s = new StringBuilder(fileName);
+                result = SystemParametersInfo(UAction.SPI_SETDESKWALLPAPER, 0, s, 0x2);
+            }
+            return result;
+        }
+
+        //public static bool SetOptions(string optionsName, string optionsData, ref string msg)
+        //{
+        //    bool returnBool = true;
+        //    RegistryKey classesRoot = Registry.CurrentUser;
+        //    RegistryKey registryKey = classesRoot.OpenSubKey(@"Control Panel\Desktop", true);
+        //    try
+        //    {
+        //        if (registryKey != null)
+        //        {
+        //            registryKey.SetValue(optionsName.ToUpper(), optionsData);
+        //        }
+        //        else
+        //        {
+        //            returnBool = false;
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        returnBool = false;
+        //        msg = "Error when read the registry";
+        //    }
+        //    finally
+        //    {
+        //        classesRoot.Close();
+        //        registryKey.Close();
+        //    }
+        //    return returnBool;
+        //}
+
+        //下载壁纸
+        private void Downpng_set()
+        {
+            notifyIcon1.Text= "正在更新";
+            try
+            {
+                if (!Directory.Exists(workingpath + "\\Picture"))
+                    Directory.CreateDirectory(workingpath + "\\Picture");
+                if (File.Exists(workingpath + "\\Picture\\temp.png"))
+                {
+                    File.Delete(workingpath + "\\Picture\\temp.png");
+                }
+                //这一行是用来下载Unsplash的壁纸的
+                System.Net.WebRequest webreq = System.Net.WebRequest.Create("https://source.unsplash.com/featured/1920x1080/?+");
+                System.Net.WebResponse webres = webreq.GetResponse();
+                Stream stream = webres.GetResponseStream();
+
+                Image drawingImage = Image.FromStream(stream);
+                stream.Dispose();
+
+                drawingImage.Save(workingpath + "\\Picture\\temp.png");
+            }
+            catch (Exception)
+            {
+            }
+            GetBackgroud();
+            SetBackgroud(workingpath + "\\Picture\\temp.png");
+            notifyIcon1.Text = "";
+        }
+        private void AutoSetWallPaper(object source, ElapsedEventArgs e)
+        {
+            Thread fetch_set = new Thread(Downpng_set);
+            fetch_set.Start();
+        }
+
+    }
+}
